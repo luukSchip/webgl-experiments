186074d1b2372e3f91cf806d543128f3ba86db66
Luuk <luuk@cell-0.com>
2015-10-15 15:01:02 +0200

    gm3

--- generalmidnight3/js/json.js
+++ generalmidnight3/js/json.js
@@ -1,3 +1,16 @@
+var verbose = false;
+
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+var source;
+
+var analyser = audioCtx.createAnalyser();
+analyser.fftSize = 2048;
+var bufferLength = analyser.frequencyBinCount;
+var dataArray = new Uint8Array(bufferLength);
+analyser.getByteTimeDomainData(dataArray);
+console.log("bufferLength " + bufferLength);
+
+
 var container;
 var camera, scene, renderer, controls, light, sea, circuitBoard, seaCam;
 var objects = new Array();
@@ -29,6 +42,15 @@ var parameters = {
 };
 var waterNormals;
 
+var api = {
+	lowerHeightBounds: -58.81455276939616,
+	lowerSpeedBounds: 3576.9156175215917,
+	randomHeight: 108.38275587001012,
+	speed: 1518.0551104637057,
+	upperHeightBounds: 6.502965352200607,
+	upperSpeedBounds: 4823.068029688207,
+	objectAmplitude: 0.2
+}
 
 
 var onProgress = function ( xhr ) {
@@ -70,17 +92,17 @@ function init() {
 	
 	
 	camera = new THREE.PerspectiveCamera( 55, window.innerWidth / window.innerHeight, 0.5, 3000000 );
-	camera.position.set( 2000, 750, 2000 );
+	camera.position.set( 20, 9, 9 );
 	controls = new THREE.OrbitControls( camera );
 	controls.addEventListener( 'change', render );
 	
   	renderer = new THREE.WebGLRenderer();
 	renderer.setPixelRatio( window.devicePixelRatio );
 	renderer.setSize( window.innerWidth, window.innerHeight );
-	renderer.shadowMap.enabled = true;
+	/*renderer.shadowMap.enabled = true;
 	renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 	renderer.shadowMapEnabled = true;
-	renderer.shadowMapType = THREE.PCFSoftShadowMap;
+	renderer.shadowMapType = THREE.PCFSoftShadowMap;*/
 	container.appendChild( renderer.domElement );
 	renderer.setClearColor( 0xffffff, 1);
 
@@ -102,12 +124,33 @@ function init() {
 		circuitBoard.traverse( function ( child ) {
 			if(child instanceof THREE.Mesh){
 				if(child.parent.name == "Plane"){
+					
+/*
+					var vertShader = document.getElementById('vertex_shh').innerHTML;
+					var fragShader = document.getElementById('fragment_shh').innerHTML;
+
+					var attributes = {}; // custom attributes
+
+					var uniforms = {    // custom uniforms (your textures)
+
+					  tOne: { type: "t", value: THREE.ImageUtils.loadTexture( "img/white.png" ) },
+					  tSec: { type: "t", value: THREE.ImageUtils.loadTexture( "img/black.png" ) }
+
+					};
+
+					var material_shh = new THREE.ShaderMaterial({
+
+					  uniforms: uniforms,
+					  attributes: attributes,
+					  vertexShader: vertShader,
+					  fragmentShader: fragShader
+
+					});
+
+					child.material = material_shh;*/
+
 					sea = child;
-					//sea.material = circuitMaterial;//.clone();
-					//sea.material = new THREE.MeshPhongMaterial({color:0x101010, side:THREE.DoubleSide});
-					//sea.material = new THREE.MeshNormalMaterial({});
-					//sea.receiveShadow = true;
-					//sea.castShadow = true;
+
 					waterGeometry = sea.geometry;
 					waterGeometry.verticesNeedUpdate = true;
 					var vertices = waterGeometry.vertices;
@@ -120,13 +163,18 @@ function init() {
 					
 				}else{
 					child.material = circuitMaterial;
+					if(child.children.length == 0){
+						objects.push(child);
+					}
 				}
 			}else if(child instanceof THREE.SpotLight){
 				light = child;
+				light.intensity = 0.2;
 			}
 		} );
 		
 		scene.add( circuitBoard );
+		animateWater();
 			
 	}, onProgress, onError );
 	
@@ -161,35 +209,63 @@ function init() {
 	skyBox = new THREE.Mesh( geometry, new THREE.MeshFaceMaterial( materials ) );
 	scene.add( skyBox );
 	
+	// GUI
+
+	initGUI();
+	initAudio();
 
 }
 
-//
+function initAudio(){
 
-function onWindowResize( event ) {
+	navigator.getUserMedia = ( navigator.getUserMedia ||
+	                       navigator.webkitGetUserMedia ||
+	                       navigator.mozGetUserMedia ||
+	                       navigator.msGetUserMedia);
+   navigator.getUserMedia (
+      // constraints: audio and video for this app
+      {
+         audio: true,
+         video: false
+      },
 
-	renderer.setSize( window.innerWidth, window.innerHeight );
+      // Success callback
+      function(stream) {
 
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
+         // Create a MediaStreamAudioSourceNode
+         // Feed the HTMLMediaElement into it
+         console.log(stream);
+         source = audioCtx.createMediaStreamSource(stream);
 
+         source.connect(analyser);
+
+      },
+
+      // Error callback
+      function(err) {
+         console.log('The following gUM error occured: ' + err);
+      }
+   );
 }
 
-//
 
-var t = 0;
-function animate() {
 
-	requestAnimationFrame( animate );
-	render();
-	if(controls){
-  		controls.update();
-	}
-}
-
-function render() {
+function initGUI() {
 	
-	if(sea){
+  var gui = new dat.GUI();
+  gui.add(api, 'speed', 1, 5000);
+  gui.add(api, 'lowerSpeedBounds', 1, 5000);
+  gui.add(api, 'upperSpeedBounds', 1, 5000);
+  gui.add(api, 'randomHeight', 0, 1000);
+  gui.add(api, 'upperHeightBounds', 0, 200);
+  gui.add(api, 'lowerHeightBounds', -100, 100);
+  gui.add(api, 'objectAmplitude', 0.0, 1);
+
+}
+
+
+function animateWater(){
+	/*if(sea){
 		waterGeometry.verticesNeedUpdate = true;
 		for(var x = 0; x < waterVertices.length; x++){
 			for(var y = 0; y < waterVertices[x].length; y++){
@@ -217,8 +293,102 @@ function render() {
 				vertex.y = meanY + (Math.random() - 0.5) * 90;
 			}
 		}
+	}*/
+
+	sea.rotateX(0.031415925765816845);
+
+	for(var x = 0; x < waterVertices.length; x++){
+		for(var y = 0; y < waterVertices[x].length; y++){
+			var vertex = waterVertices[x][y];
+			tweenVertex(vertex,x,y);
+		}
 	}
-	camera.position.x -= 0.001;
+}
+
+function tweenVertex(vertex,x,y){
+	var neighbours = new Array();
+	var totalY = vertex.y;
+	if(x > 0){
+		neighbours.push(waterVertices[x - 1][y]);
+		totalY += waterVertices[x - 1][y].y;
+	}if(y < WATER_GRID_SIZE - 1){
+		neighbours.push(waterVertices[x][y + 1]);
+		try{
+			totalY += waterVertices[x][y + 1].y;
+		}catch(e){
+			//console.log(x + ", " + y);
+		}
+	}if(x < WATER_GRID_SIZE - 1){
+		neighbours.push(waterVertices[x + 1][y]);
+		totalY += waterVertices[x + 1][y].y;
+	}if(y > 0){
+		neighbours.push(waterVertices[x][y - 1]);
+		totalY += waterVertices[x][y - 1].y;
+	}
+	var meanY = totalY / (neighbours.length + 1);
+	//console.log(meanY);
+	var newY = meanY + (Math.random() - 0.5) * api.randomHeight;
+	newY = Math.max(newY, api.lowerHeightBounds);
+	newY = Math.min(newY, api.upperHeightBounds);
+	newY = newY * x / 10;
+
+	var speed = Math.random() * api.speed;
+	speed = Math.max(speed, api.lowerSpeedBounds);
+	speed = Math.min(speed, api.upperSpeedBounds);
+
+	// var speed = 5000;
+
+	new TWEEN.Tween({y:vertex.y})
+	    .to({y:newY}, Math.random() * speed)
+	    .onUpdate(function(){
+			waterGeometry.verticesNeedUpdate = true;
+	        vertex.y = this.y;
+	    }).onComplete(function(){
+	    	setTimeout(function(){
+	    		tweenVertex(vertex,x,y);
+	    	},1);
+	    }).start();
+}
+
+//
+
+function onWindowResize( event ) {
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+}
+
+//
+
+var t = 0;
+function animate() {
+
+	requestAnimationFrame( animate );
+	render();
+	if(controls){
+  		controls.update();
+	}
+    TWEEN.update();
+}
+
+function render() {
+
+	//camera.position.x -= 0.001;
 	renderer.render( scene, camera );
 	
+	analyser.getByteFrequencyData(dataArray);
+	for(var i = 0; i < objects.length; i++){
+		var object = objects[i];
+		if(object.parent.name != "Plane.2"){
+			object.scale.y = Math.max(0.1,parseFloat(dataArray[parseInt(i*parseFloat(dataArray.length/objects.length))]) * api.objectAmplitude);
+		}
+	}
+	if(verbose){
+		console.log(dataArray);
+		console.log(objects);
+	}
+	verbose = false;
 }
